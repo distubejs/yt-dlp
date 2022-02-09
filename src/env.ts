eb4d3a494c2f52725bd3a9174b0dd6dc4d2f70ae
Skick <skick1234@gmail.com>
2022-02-09 11:54:13 +0700

    ci: publish without including binary `yt-dlp` file

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ const e = (key: string) => process.env[key];
 export const YTDLP_DISABLE_DOWNLOAD = !!e("YTDLP_DISABLE_DOWNLOAD");
 export const YTDLP_URL = e("YTDLP_URL") || "https://api.github.com/repos/yt-dlp/yt-dlp/releases?per_page=1";
 export const YTDLP_IS_WINDOWS = e("YTDLP_IS_WINDOWS") || process.platform === "win32";
-export const YTDLP_DIR = e("YTDLP_DIR") || path.join(__dirname, "..");
+export const YTDLP_DIR = e("YTDLP_DIR") || path.join(__dirname, "..", "bin");
 export const YTDLP_FILENAME = e("YTDLP_FILENAME") || `yt-dlp${YTDLP_IS_WINDOWS ? ".exe" : ""}`;
 export const YTDLP_PATH = path.join(YTDLP_DIR, YTDLP_FILENAME);
 
